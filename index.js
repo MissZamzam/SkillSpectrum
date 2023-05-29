fa91a1e37250efcfd7cf976dc246d909ac206809
Zamzam <Zamzamhassan44@gmail.com>
2023-05-30 00:09:12 +0300

    finalised FAQ section accordion

--- index.js
+++ index.js
@@ -4,3 +4,22 @@ window.addEventListener('scroll',() => {
 document.querySelector('nav').classList.toggle
 ('window-scroll', window.scrollY > 0)
 })
+
+
+// show faq reponse(paragraph)
+
+const faqs = document.querySelectorAll('.faq');
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open')
+
+
+        // change icon after it opens
+        const icon = faq.querySelector('.faq__icon i');
+        if(icon.className === 'uil uil-plus'){
+            icon.className = 'uil uil-minus'
+        }else{
+            icon.className = 'uil uil-plus'
+        }
+    })
+})
