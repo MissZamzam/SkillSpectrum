b22fe162448ce5390ae666efeb56c1d8426ab864
Zamzam <Zamzamhassan44@gmail.com>
2023-05-31 01:16:59 +0300

    nav responsiveness to smaller screen(tablets) with animations

--- index.js
+++ index.js
@@ -23,3 +23,22 @@ faqs.forEach(faq => {
         }
     })
 })
+
+// nav-menu hide/show
+const menu = document.querySelector(".nav_menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+menuBtn.addEventListener('click', () => {
+    menu.style.display = "flex";
+    closeBtn.style.display = "inline-block";    
+    menuBtn.style.display = "none";
+})
+
+// close nav menu
+const closeNav = () => {
+    menu.style.display = "none";
+    closeBtn.style.display = "none";    
+    menuBtn.style.display = "inline-block";
+}
+closeBtn.addEventListener('click', closeNav)
